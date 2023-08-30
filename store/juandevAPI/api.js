d59c82e62e891420d314213e5905f5e7724aaba4
Crowdbotics <engineering@crowdbotics.com>
2023-08-30 18:16:41 +0000

    Update store.

--- store/juandevAPI/api.js
+++ store/juandevAPI/api.js
@@ -1,168 +1,140 @@
-import axios from "axios";
+import axios from "axios"
 const juandevAPI = axios.create({
   baseURL: "https://juan02082023-dev-106627.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return juandevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return juandevAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_azul_list(payload) {
-  return juandevAPI.get(`/api/v1/azul/`);
+  return juandevAPI.get(`/api/v1/azul/`)
 }
-
 function api_v1_azul_create(payload) {
-  return juandevAPI.post(`/api/v1/azul/`, payload);
+  return juandevAPI.post(`/api/v1/azul/`, payload)
 }
-
 function api_v1_azul_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/azul/${payload.id}/`);
+  return juandevAPI.get(`/api/v1/azul/${payload.id}/`)
 }
-
 function api_v1_azul_update(payload) {
-  return juandevAPI.put(`/api/v1/azul/${payload.id}/`, payload);
+  return juandevAPI.put(`/api/v1/azul/${payload.id}/`, payload)
 }
-
 function api_v1_azul_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/azul/${payload.id}/`, payload);
+  return juandevAPI.patch(`/api/v1/azul/${payload.id}/`, payload)
 }
-
 function api_v1_azul_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/azul/${payload.id}/`);
+  return juandevAPI.delete(`/api/v1/azul/${payload.id}/`)
 }
-
 function api_v1_login_create(payload) {
-  return juandevAPI.post(`/api/v1/login/`, payload);
+  return juandevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_newmo_list(payload) {
-  return juandevAPI.get(`/api/v1/newmo/`);
+  return juandevAPI.get(`/api/v1/newmo/`)
 }
-
 function api_v1_newmo_create(payload) {
-  return juandevAPI.post(`/api/v1/newmo/`, payload);
+  return juandevAPI.post(`/api/v1/newmo/`, payload)
 }
-
 function api_v1_newmo_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/newmo/${payload.id}/`);
+  return juandevAPI.get(`/api/v1/newmo/${payload.id}/`)
 }
-
 function api_v1_newmo_update(payload) {
-  return juandevAPI.put(`/api/v1/newmo/${payload.id}/`, payload);
+  return juandevAPI.put(`/api/v1/newmo/${payload.id}/`, payload)
 }
-
 function api_v1_newmo_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/newmo/${payload.id}/`, payload);
+  return juandevAPI.patch(`/api/v1/newmo/${payload.id}/`, payload)
 }
-
 function api_v1_newmo_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/newmo/${payload.id}/`);
+  return juandevAPI.delete(`/api/v1/newmo/${payload.id}/`)
 }
-
 function api_v1_newmo45_list(payload) {
-  return juandevAPI.get(`/api/v1/newmo45/`);
+  return juandevAPI.get(`/api/v1/newmo45/`)
 }
-
 function api_v1_newmo45_create(payload) {
-  return juandevAPI.post(`/api/v1/newmo45/`, payload);
+  return juandevAPI.post(`/api/v1/newmo45/`, payload)
 }
-
 function api_v1_newmo45_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/newmo45/${payload.id}/`);
+  return juandevAPI.get(`/api/v1/newmo45/${payload.id}/`)
 }
-
 function api_v1_newmo45_update(payload) {
-  return juandevAPI.put(`/api/v1/newmo45/${payload.id}/`, payload);
+  return juandevAPI.put(`/api/v1/newmo45/${payload.id}/`, payload)
 }
-
 function api_v1_newmo45_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/newmo45/${payload.id}/`, payload);
+  return juandevAPI.patch(`/api/v1/newmo45/${payload.id}/`, payload)
 }
-
 function api_v1_newmo45_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/newmo45/${payload.id}/`);
+  return juandevAPI.delete(`/api/v1/newmo45/${payload.id}/`)
 }
-
 function api_v1_signup_create(payload) {
-  return juandevAPI.post(`/api/v1/signup/`, payload);
+  return juandevAPI.post(`/api/v1/signup/`, payload)
 }
-
+function api_v1_vik_list(payload) {
+  return juandevAPI.get(`/api/v1/vik/`)
+}
+function api_v1_vik_create(payload) {
+  return juandevAPI.post(`/api/v1/vik/`, payload)
+}
+function api_v1_vik_retrieve(payload) {
+  return juandevAPI.get(`/api/v1/vik/${payload.id}/`)
+}
+function api_v1_vik_update(payload) {
+  return juandevAPI.put(`/api/v1/vik/${payload.id}/`, payload)
+}
+function api_v1_vik_partial_update(payload) {
+  return juandevAPI.patch(`/api/v1/vik/${payload.id}/`, payload)
+}
+function api_v1_vik_destroy(payload) {
+  return juandevAPI.delete(`/api/v1/vik/${payload.id}/`)
+}
 function modules_articles_article_list(payload) {
-  return juandevAPI.get(`/modules/articles/article/`);
+  return juandevAPI.get(`/modules/articles/article/`)
 }
-
 function modules_articles_article_create(payload) {
-  return juandevAPI.post(`/modules/articles/article/`, payload);
+  return juandevAPI.post(`/modules/articles/article/`, payload)
 }
-
 function modules_articles_article_retrieve(payload) {
-  return juandevAPI.get(`/modules/articles/article/${payload.id}/`);
+  return juandevAPI.get(`/modules/articles/article/${payload.id}/`)
 }
-
 function modules_articles_article_update(payload) {
-  return juandevAPI.put(`/modules/articles/article/${payload.id}/`, payload);
+  return juandevAPI.put(`/modules/articles/article/${payload.id}/`, payload)
 }
-
 function modules_articles_article_partial_update(payload) {
-  return juandevAPI.patch(`/modules/articles/article/${payload.id}/`, payload);
+  return juandevAPI.patch(`/modules/articles/article/${payload.id}/`, payload)
 }
-
 function modules_articles_article_destroy(payload) {
-  return juandevAPI.delete(`/modules/articles/article/${payload.id}/`);
+  return juandevAPI.delete(`/modules/articles/article/${payload.id}/`)
 }
-
 function rest_auth_login_create(payload) {
-  return juandevAPI.post(`/rest-auth/login/`, payload);
+  return juandevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return juandevAPI.get(`/rest-auth/logout/`);
+  return juandevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return juandevAPI.post(`/rest-auth/logout/`);
+  return juandevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/change/`, payload);
+  return juandevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/reset/`, payload);
+  return juandevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return juandevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return juandevAPI.post(`/rest-auth/registration/`, payload);
+  return juandevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return juandevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return juandevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return juandevAPI.get(`/rest-auth/user/`);
+  return juandevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return juandevAPI.put(`/rest-auth/user/`, payload);
+  return juandevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return juandevAPI.patch(`/rest-auth/user/`, payload);
+  return juandevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_azul_list,
@@ -185,6 +157,12 @@ export const apiService = {
   api_v1_newmo45_partial_update,
   api_v1_newmo45_destroy,
   api_v1_signup_create,
+  api_v1_vik_list,
+  api_v1_vik_create,
+  api_v1_vik_retrieve,
+  api_v1_vik_update,
+  api_v1_vik_partial_update,
+  api_v1_vik_destroy,
   modules_articles_article_list,
   modules_articles_article_create,
   modules_articles_article_retrieve,
@@ -202,4 +180,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
--- store/juandevAPI/viks.slice.js
+++ store/juandevAPI/viks.slice.js
@@ -1,94 +1,94 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const api_v1_newmo45_list = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_list",
+export const api_v1_vik_list = createAsyncThunk(
+  "viks/api_v1_vik_list",
   async payload => {
-    const response = await apiService.api_v1_newmo45_list(payload)
+    const response = await apiService.api_v1_vik_list(payload)
     return response.data
   }
 )
-export const api_v1_newmo45_create = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_create",
+export const api_v1_vik_create = createAsyncThunk(
+  "viks/api_v1_vik_create",
   async payload => {
-    const response = await apiService.api_v1_newmo45_create(payload)
+    const response = await apiService.api_v1_vik_create(payload)
     return response.data
   }
 )
-export const api_v1_newmo45_retrieve = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_retrieve",
+export const api_v1_vik_retrieve = createAsyncThunk(
+  "viks/api_v1_vik_retrieve",
   async payload => {
-    const response = await apiService.api_v1_newmo45_retrieve(payload)
+    const response = await apiService.api_v1_vik_retrieve(payload)
     return response.data
   }
 )
-export const api_v1_newmo45_update = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_update",
+export const api_v1_vik_update = createAsyncThunk(
+  "viks/api_v1_vik_update",
   async payload => {
-    const response = await apiService.api_v1_newmo45_update(payload)
+    const response = await apiService.api_v1_vik_update(payload)
     return response.data
   }
 )
-export const api_v1_newmo45_partial_update = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_partial_update",
+export const api_v1_vik_partial_update = createAsyncThunk(
+  "viks/api_v1_vik_partial_update",
   async payload => {
-    const response = await apiService.api_v1_newmo45_partial_update(payload)
+    const response = await apiService.api_v1_vik_partial_update(payload)
     return response.data
   }
 )
-export const api_v1_newmo45_destroy = createAsyncThunk(
-  "newMo45s/api_v1_newmo45_destroy",
+export const api_v1_vik_destroy = createAsyncThunk(
+  "viks/api_v1_vik_destroy",
   async payload => {
-    const response = await apiService.api_v1_newmo45_destroy(payload)
+    const response = await apiService.api_v1_vik_destroy(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const newMo45sSlice = createSlice({
-  name: "newMo45s",
+const viksSlice = createSlice({
+  name: "viks",
   initialState,
   reducers: {},
   extraReducers: builder => {
     builder
-      .addCase(api_v1_newmo45_list.pending, (state, action) => {
+      .addCase(api_v1_vik_list.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_list.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_list.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities = action.payload
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_list.rejected, (state, action) => {
+      .addCase(api_v1_vik_list.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_create.pending, (state, action) => {
+      .addCase(api_v1_vik_create.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_create.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_create.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities.push(action.payload)
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_create.rejected, (state, action) => {
+      .addCase(api_v1_vik_create.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_retrieve.pending, (state, action) => {
+      .addCase(api_v1_vik_retrieve.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_retrieve.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_retrieve.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities = [
             ...state.entities.filter(record => record.id !== action.payload.id),
@@ -97,18 +97,18 @@ const newMo45sSlice = createSlice({
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_retrieve.rejected, (state, action) => {
+      .addCase(api_v1_vik_retrieve.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_update.pending, (state, action) => {
+      .addCase(api_v1_vik_update.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_update.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_update.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities = state.entities.map(record =>
             record.id === action.payload.id ? action.payload : record
@@ -116,18 +116,18 @@ const newMo45sSlice = createSlice({
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_update.rejected, (state, action) => {
+      .addCase(api_v1_vik_update.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_partial_update.pending, (state, action) => {
+      .addCase(api_v1_vik_partial_update.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_partial_update.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_partial_update.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities = state.entities.map(record =>
             record.id === action.payload.id ? action.payload : record
@@ -135,18 +135,18 @@ const newMo45sSlice = createSlice({
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_partial_update.rejected, (state, action) => {
+      .addCase(api_v1_vik_partial_update.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_destroy.pending, (state, action) => {
+      .addCase(api_v1_vik_destroy.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
         }
       })
-      .addCase(api_v1_newmo45_destroy.fulfilled, (state, action) => {
+      .addCase(api_v1_vik_destroy.fulfilled, (state, action) => {
         if (state.api.loading === "pending") {
           state.entities = state.entities.filter(
             record => record.id !== action.meta.arg?.id
@@ -154,7 +154,7 @@ const newMo45sSlice = createSlice({
           state.api.loading = "idle"
         }
       })
-      .addCase(api_v1_newmo45_destroy.rejected, (state, action) => {
+      .addCase(api_v1_vik_destroy.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
@@ -163,11 +163,11 @@ const newMo45sSlice = createSlice({
   }
 })
 export default {
-  api_v1_newmo45_list,
-  api_v1_newmo45_create,
-  api_v1_newmo45_retrieve,
-  api_v1_newmo45_update,
-  api_v1_newmo45_partial_update,
-  api_v1_newmo45_destroy,
-  slice: newMo45sSlice
+  api_v1_vik_list,
+  api_v1_vik_create,
+  api_v1_vik_retrieve,
+  api_v1_vik_update,
+  api_v1_vik_partial_update,
+  api_v1_vik_destroy,
+  slice: viksSlice
 }
